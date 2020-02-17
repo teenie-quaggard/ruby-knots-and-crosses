f9724584794e61f6ab9b47c598490f2524a21602
Kristina Jaggard <kjaggard@8thlight.com>
2020-02-17 20:42:34 +0000

    add basic hard-coded 3x3 board

--- lib/game.rb
+++ lib/game.rb
@@ -3,16 +3,14 @@ require "game/version"
 module Game
   WELCOME_MESSAGE = "Hello sunshine. Welcome to Noughts and Crosses."
   GAME_RULES = "These are the rules of the game:"
+  BOARD = "\n\n1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9\n\n\n"
 
-  class PrintsMessageToUser
+  class Display
     def self.prints_message(msg)
       puts msg
       msg
     end
   end
 
-  # class Board
-    
-  # end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,17 +8,17 @@ RSpec.describe Game do
 
 end
 
-RSpec.describe Game::PrintsMessageToUser do
+RSpec.describe Game::Display do
   it "prints statement" do
-    WELCOME_MESSAGE = "Hello sunshine. Welcome to Knots and Crosses."
+    WELCOME_MESSAGE = "Hello sunshine. Welcome to Noughts and Crosses."
     GAME_RULES = "These are the rules of the game."
-    expect(Game::PrintsMessageToUser.prints_message(WELCOME_MESSAGE)).to eql("Hello sunshine. Welcome to Knots and Crosses.")
-    expect(Game::PrintsMessageToUser.prints_message(GAME_RULES)).to eql("These are the rules of the game.")
+    expect(Game::Display.prints_message(WELCOME_MESSAGE)).to eql("Hello sunshine. Welcome to Noughts and Crosses.")
+    expect(Game::Display.prints_message(GAME_RULES)).to eql("These are the rules of the game.")
 
   end
 
   it "prints board" do
-    BOARD = '1|2|3 /n 4|5|6 /n 7/8/9'
-    expect(Game::PrintsMessageToUser.prints_message(BOARD)).to eql('1|2|3 /n 4|5|6 /n 7|8|9')
+    BOARD = "\n\n1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9\n\n\n"
+    expect(Game::Display.prints_message(BOARD)).to eql("\n\n1 | 2 | 3\n---------\n4 | 5 | 6\n---------\n7 | 8 | 9\n\n\n")
   end
 end
