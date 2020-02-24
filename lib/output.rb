8c625c043f90e38a5471641a3d5ccc7591f041c2
Kristina Jaggard <kjaggard@8thlight.com>
2020-02-24 20:35:13 +0000

    create dumb #print method in output

--- lib/output.rb
+++ lib/output.rb
@@ -3,6 +3,8 @@ class Output
         puts "Howdy stranger. Welcome to Noughts and Crosses."
     end
 
-
+    def board 
+        puts " 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 \n"
+    end
     
 end
--- spec/output_spec.rb
+++ spec/output_spec.rb
@@ -13,9 +13,11 @@ RSpec.describe Output do
     describe "#board" do
         it "print the current board" do
             output = Output.new()
+
+            puts output.board
             expect do 
                 output.board
-            end.to output("Howdy stranger. Welcome to Noughts and Crosses.\n").to_stdout
+            end.to output(" 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 \n").to_stdout
         end
     end
 
